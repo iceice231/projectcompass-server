ae83b6394ccb6c0478c50dba33122a5f8a467ab9
iceice231 <si4ev013@gmail.com>
2024-05-10 21:32:42 +0300

    Проверка

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ const app = express()
 
 app.use(cors(
     {origin: "https://projectcompass-server-production.up.railway.app"}))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://projectcompass-server-production.up.railway.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({limit: '50mb'}));
 app.use('/uploads', express.static('./uploads'))
