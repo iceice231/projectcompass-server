aafca90872f6183dea559d421b012aa8b815d89c
iceice231 <si4ev013@gmail.com>
2024-05-10 20:53:49 +0300

    Проверка

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ import './loadEnv.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({origin: true}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({limit: '50mb'}));
 app.use('/uploads', express.static('./uploads'))
